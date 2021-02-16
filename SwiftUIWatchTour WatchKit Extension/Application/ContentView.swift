5dd575017d91c3933fa57c26cc836cf3f3d4e41c
Angel Newton <xanewton@gmail.com>
2021-02-15 19:53:06 -0500

    Add the main navigation

--- SwiftUIWatchTour WatchKit Extension/Application/ContentView.swift
+++ SwiftUIWatchTour WatchKit Extension/Application/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresented = false
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        List {
+            Button("Charts") {
+                self.isPresented.toggle()
+            }
+            .fullScreenCover(isPresented: $isPresented, content:       ChartsView.init)
+            
+            NavigationLink(destination: ColorsView()) {
+                Text("Colors")
+            }.navigationTitle("Home")
+        }
     }
 }
 
